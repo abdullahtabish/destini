6936511ff589a287ccd0613bc7972cc37c3aedf4
Abdullah Tabish <70273959+abdullahtabish@users.noreply.github.com>
2021-11-13 14:27:12 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/count/com/destini/MainActivity.kt
@@ -0,0 +1,6 @@
+package count.com.destini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,5 @@
+class Story {
+  late String storyTitle, choice1, choice2;
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
+}
